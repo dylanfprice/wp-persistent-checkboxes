e58c4b72a9bd9d0e67868afa67f890c932b98755
Dylan Price <the.dylan.price@gmail.com>
2018-09-29 20:37:09 -0700

    Remove line break.

--- index.php
+++ index.php
@@ -2,8 +2,7 @@
 /**
  * Plugin Name: Persistent Checkboxes
  * Plugin URI: https://github.com/dylanfprice/wp-persistent-checkboxes
- * Description: This plugin contains a gutenberg block that renders checkboxes
- *              whose state will be persisted in the end user's browser.
+ * Description: This plugin contains a gutenberg block that renders checkboxes whose state will be persisted in the end user's browser.
  * Version: 0.0.1
  * Author: Dylan Price <the.dylan.price@gmail.com>
  *

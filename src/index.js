23de12ffca2795fb17b06d758d124915fdfeca59
Dylan Price <the.dylan.price@gmail.com>
2018-10-02 10:09:41 -0700

    Start implementing with correct approach.

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import {CheckboxControl} from '@wordpress/components'
 import md5 from 'md5'
+import ReactDOM from 'react-dom'
 import React from 'react'
 import {registerBlockType} from '@wordpress/blocks'
 import {RichText} from '@wordpress/editor'
@@ -12,8 +13,8 @@ registerBlockType('persistent-checkboxes/persistent-checkboxes', {
         'Block showing a list of checkboxes and labels. When an end user ' +
         'checks a box it is remembered across refreshes in their browser.'
     ),
-    icon: '',
-    category: '',
+    icon: 'yes',
+    category: 'widgets',
     attributes: {
         labels: {
             type: 'array',
@@ -21,7 +22,7 @@ registerBlockType('persistent-checkboxes/persistent-checkboxes', {
             selector: LABEL_SELECTOR,
         },
     },
-    edit: ({className, attributes: {labels}, setAttributes}) => {
+    edit: ({className, attributes: {labels = ['Edit me!']}, setAttributes}) => {
         const checkboxes = labels.map(label =>
             <CheckboxControl
                 key={label}
@@ -40,33 +41,92 @@ registerBlockType('persistent-checkboxes/persistent-checkboxes', {
             </div>
         )
     },
-    save: ({className, attributes: {labels}}) => {
-        const listId = `list-${md5(labels.join('\n'))}`
-
-        const checkboxes = labels.map((label) => {
-            const checkboxId = `${listId}-checkbox-${md5(label)}`
-            return (
-                <CheckboxControl
-                    key={label}
-                    label={label}
-                    checked={isChecked(checkboxId)}
-                    onChange={checked =>
-                        onChangeCheckboxControl(checkboxId, checked)
-                    }
-                />
-            )
-        })
-
+    save: (props) => {
+        const {attributes: {labels}} = props
+        const listId = getListId(labels)
         return (
-            <div className={className}>
-                <RichText.Content value={checkboxes} />
+            <div id={listId}>
+                <script>
+                    window.wp.persistentCheckboxes.render({JSON.stringify(props)})
+                </script>
             </div>
         )
     },
 })
 
 function extractLabel (checkboxControl) {
-    return checkboxControl.querySelector(LABEL_SELECTOR).innerHTML
+    return checkboxControl['props']['children'][0]['props']['children'][1]['props']['children'][0]
+}
+
+export function render (props) {
+    const element = React.createElement(PersistentCheckboxList, props)
+    const listId = getListId(props.attributes.labels)
+    ReactDOM.render(element, document.getElementById(listId))
+}
+
+class PersistentCheckboxList extends React.Component {
+    constructor(props) {
+        super(props)
+    }
+    render () {
+        const {className, attributes: {labels}} = this.props
+        const listId = getListId(labels)
+
+        const checkboxes = labels.map(label => {
+            const checkboxId = `${listId}-checkbox-${md5(label)}`
+            const checked = isChecked(checkboxId)
+            const style = checked ? {textDecoration: 'line-through'} : {}
+            return (
+                <CheckboxControl
+                    key={checkboxId}
+                    style={style}
+                    label={label}
+                    checked={checked}
+                    onChange={checked => {
+                        onChangeCheckboxControl(checkboxId, checked)
+                    }}
+                />
+            )
+        })
+        return (
+            <div className={className}>
+                {checkboxes}
+            </div>
+        )
+    }
+}
+
+class PersistentCheckbox extends React.Component {
+    constructor(props) {
+        super(props)
+        this.state = {checked: true}
+        this.onChange = this.onChange.bind(this);
+    }
+
+    onChange(checked) {
+        const checkboxId = this.props.id
+        let checkboxValues = getCheckboxValues()
+        checkboxValues[id] = checked
+        saveCheckboxValues(checkboxValues)
+
+    }
+
+    render () {
+        const checked = isChecked(this.props.id)
+        const style = checked ? {textDecoration: 'line-through'} : {}
+        return (
+            <CheckboxControl
+                style={style}
+                label={this.props.label}
+                checked={checked}
+                onChange={this.onChange}
+            />
+        )
+    }
+}
+
+function getListId (labels) {
+    return `list-${md5(labels.join('\n'))}`
 }
 
 function getKey () {
@@ -82,9 +142,6 @@ function saveCheckboxValues (checkboxValues) {
 }
 
 function onChangeCheckboxControl (id, checked) {
-    let checkboxValues = getCheckboxValues()
-    checkboxValues[id] = checked
-    saveCheckboxValues(checkboxValues)
 }
 
 function isChecked (id) {

dbb3efd481e43be7c97f4927e1e0fc9948915211
Dylan Price <the.dylan.price@gmail.com>
2018-12-11 10:14:12 -0800

    Switch plugin url to bitbucket.

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * Plugin Name: Persistent Checkboxes
- * Plugin URI: https://github.com/dylanfprice/wp-persistent-checkboxes
+ * Plugin URI: https://bitbucket.org/dylanfprice/wp-persistent-checkboxes
  * Description: This plugin contains a gutenberg block that renders checkboxes whose state will be persisted in the end user's browser.
  * Version: 0.0.1
  * Author: Dylan Price <the.dylan.price@gmail.com>

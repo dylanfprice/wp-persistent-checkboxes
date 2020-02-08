bcf610529696769564770ac1435f9b039af1229b
Dylan Price <the.dylan.price@gmail.com>
2020-02-07 21:48:25 -0800

    Clean up plugin headers and add GPLv2 license.

--- index.php
+++ index.php
@@ -2,9 +2,9 @@
 /**
  * Plugin Name: Persistent Checkboxes
  * Plugin URI: https://bitbucket.org/dylanfprice/wp-persistent-checkboxes
- * Description: This plugin contains a gutenberg block that renders checkboxes whose state will be persisted in the end user's browser.
+ * Description: Gutenberg block that renders checkboxes whose state will be persisted in the end user's browser.
  * Version: 0.1.0
- * Author: Dylan Price <the.dylan.price@gmail.com>
+ * Author: Dylan Price
  *
  * @package persistent-checkboxes
  */
